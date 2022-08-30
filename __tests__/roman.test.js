e0e28caa4b4bec80ab2d796dda7ec0cee0957f3b
josephjacksonw <josephjacksonw@gmail.com>
2022-08-30 13:56:42 -0400

    good pass for roman()
    Co-authored-by: Athea DeLing <almdeling@gmail.com>

--- __tests__/roman.test.js
+++ __tests__/roman.test.js
@@ -7,5 +7,23 @@ describe('roman', () => {
     expect(roman(input)).toEqual("X")
   })
 
+  test('should return M for every 1000 inputted', () => {
+    const input = 2000
+    expect(roman(input)).toEqual("MM")
+  })
+
+  test('should return CM for every 900 inputted', () => {
+    const input = 900
+    expect(roman(input)).toEqual("CM")
+  })
+
+  test('should return D for every 500 inputted', () => {
+    const input = 2500
+    expect(roman(input)).toEqual("MMD")
+  })
+  test('should return CD for every 400 inputted', () => {
+    const input = 1432
+    expect(roman(input)).toEqual("MCDXXXII")
+  })
 
 })
--- src/roman.js
+++ src/roman.js
@@ -1,4 +1,57 @@
 export default function roman(i) {
   let roman = ""
+  let numRemaining = i
+  while (numRemaining >= 1000) {
+    roman += "M"
+    numRemaining -= 1000
+  }
+  while (numRemaining >= 900) {
+    roman += "CM"
+    numRemaining -= 900
+  }
+  while (numRemaining >= 500) {
+    roman += "D"
+    numRemaining -= 500
+  }
+  while (numRemaining >= 400) {
+    roman += "CD"
+    numRemaining -= 400
+  }
+  while (numRemaining >= 100) {
+    roman += "C"
+    numRemaining -= 100
+  }
+  while (numRemaining >= 90) {
+    roman += "XC"
+    numRemaining -= 90
+  }
+  while (numRemaining >= 50) {
+    roman += "L"
+    numRemaining -= 50
+  }
+  while (numRemaining >= 40) {
+    roman += "XL"
+    numRemaining -= 40
+  }
+  while (numRemaining >= 10) {
+    roman += "X"
+    numRemaining -= 10
+  }
+  while (numRemaining >= 9) {
+    roman += "IX"
+    numRemaining -= 9
+  }
+  while (numRemaining >= 5) {
+    roman += "V"
+    numRemaining -= 5
+  }
+  while (numRemaining >= 4) {
+    roman += "IV"
+    numRemaining -= 4
+  }
+  while (numRemaining >= 1) {
+    roman += "I"
+    numRemaining -= 1
+  }
   return roman
 }
