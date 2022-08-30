26f4fee8f969eb06c649bd30f271023de83917a4
josephjacksonw <josephjacksonw@gmail.com>
2022-08-30 14:31:54 -0400

    Built user logic and html for site
    Co-authored-by: Athea DeLing <almdeling@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import numCheck from './numeral';
+import roman from './roman';
 
+function handleForm(e) {
+  e.preventDefault();
+  let input = document.getElementById("numInput").value;
+  let output = "";
+  if (numCheck(input) != input) {
+    output = "Error, input is not a number";
+  } else {
+    output = roman(input);
+  }
+  document.getElementById("result").innerText = output;
+}
 
+document.querySelector("form#number").addEventListener("submit", handleForm);
